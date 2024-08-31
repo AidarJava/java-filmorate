8d9d935693fb9575afebd5b33e7ec77459e0cf6a
Aidar <Abdulnagimov-AI@yandex.ru>
2024-08-31 13:57:42 +0500

    Финальный проект спринта 10

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,100 @@
 package ru.yandex.practicum.filmorate;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.ecxeption.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.controller.UserController;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 @SpringBootTest
-class FilmorateApplicationTests {
+public class FilmorateApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Test(expected = ValidationException.class)
+	public void testValidateEmailUser() throws IOException {
+
+		User user = new User();
+		user.setId(1L);
+		user.setEmail("exampleexample.com");
+		user.setLogin("exampleLogin");
+		user.setName("Example");
+		user.setBirthday(LocalDate.of(2000, 1, 1));
+		UserController userController = new UserController();
+		userController.createUser(user);
+	}
+
+	@Test(expected = ValidationException.class)
+	public void testValidateLoginUser() throws IOException {
+
+		User user = new User();
+		user.setId(1L);
+		user.setEmail("example@example.com");
+		user.setLogin("");
+		user.setName("Example");
+		user.setBirthday(LocalDate.of(2000, 1, 1));
+		UserController userController = new UserController();
+		userController.createUser(user);
+	}
+
+	@Test(expected = ValidationException.class)
+	public void testValidateDateUser() throws IOException {
+
+		User user = new User();
+		user.setId(1L);
+		user.setEmail("example@example.com");
+		user.setLogin("exampleLogin");
+		user.setName("Example");
+		user.setBirthday(LocalDate.of(2050, 1, 1));
+		UserController userController = new UserController();
+		userController.createUser(user);
+	}
+
+	@Test(expected = ValidationException.class)
+	public void testValidateFilmName() throws IOException {
+		Film film = new Film();
+		film.setName("");
+		film.setDescription("exampleFilm");
+		film.setReleaseDate(LocalDate.of(2000, 1, 1));
+		film.setDuration(100);
+		FilmController filmController = new FilmController();
+		filmController.createFilm(film);
+	}
+
+	@Test(expected = ValidationException.class)
+	public void testValidateFilmDescription() throws IOException {
+		Film film = new Film();
+		film.setName("Example");
+		film.setDescription("exampleFilmexampleFilmexampleFilmexampleFilmexampleFilmexampleFilmexampleFilmexampleFilmexampleFilmexampleFilmexampleFilmexampleFilmexampleFilmexampleFilmexampleFilmexampleFilmexampleFilmFilmexampleFilmFilmexampleFilmFilmexampleFilmFilmexampleFilmFilmexampleFilmFilmexampleFilm");
+		film.setReleaseDate(LocalDate.of(2000, 1, 1));
+		film.setDuration(100);
+		FilmController filmController = new FilmController();
+		filmController.createFilm(film);
+	}
+
+	@Test(expected = ValidationException.class)
+	public void testValidateFilmRelease() throws IOException {
+		Film film = new Film();
+		film.setName("Example");
+		film.setDescription("exampleFilm");
+		film.setReleaseDate(LocalDate.of(1895, 12, 27));
+		film.setDuration(100);
+		FilmController filmController = new FilmController();
+		filmController.createFilm(film);
+	}
+
+	@Test(expected = ValidationException.class)
+	public void testValidateFilmDuration() throws IOException {
+		Film film = new Film();
+		film.setName("Example");
+		film.setDescription("exampleFilm");
+		film.setReleaseDate(LocalDate.of(2000, 1, 1));
+		film.setDuration(0);
+		FilmController filmController = new FilmController();
+		filmController.createFilm(film);
 	}
 
 }
